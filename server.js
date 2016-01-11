e6ef1bdeaf9d29f43715dece3e9bca235fd26b9d
Brandon <railsdev14@gmail.com>
2016-01-11 14:11:47 -0500

    Added mongoose connect call

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// BASE SETUP
 // Calling Packages
 var express = require('express'); // call express
 var app = express(); // define our app using express
@@ -5,6 +6,9 @@ var bodyParser = require('body-parser'); // get body-parser
 var morgan = require('morgan'); // used to see requests
 var mongoose = require('mongoose'); // for working w/ our database
 var port = process.env.PORT || 8080; // set the port for our app
+
+// Connecting to our database (locally)
+mongoose.connect('mongodb://localhost:27017/myDatabase');
 
 // App Configuration
 // use body parser to grab info from POST requests
@@ -43,3 +47,5 @@ app.use('/api', apiRouter);
 // Start the server
 app.listen(port);
 console.log('Magic is happening on port ' + port);
+
+// start server with command: $ nodemon server.js
